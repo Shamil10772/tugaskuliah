73ba3e8b80a060c789dd52d28868aa993fb4291c
Shamil <shamilyassin799@gmail.com>
2024-02-29 20:12:56 +0700

    Membuat file "LoginController" "Login_form" dan routes

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if (Auth::attempt(['email' => $request->email, 'password' => $request->password], $request->remember)) {
+            return redirect()->intended('/home');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
+Route::group(['middleware' => 'web'], function () {
+    // ... lainnya
+
+    Auth::routes();
+
+    // ... lainnya
+});
+
 // Route::get('/listbarang/{id}/{nama}', function ($id, $nama){
 //     return view('list_barang', compact('id', 'nama'));
 // });
 
+Auth::routes(['login', 'password.reset']);
 Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
