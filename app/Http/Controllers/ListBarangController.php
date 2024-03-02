55d11122b4c9fb87a6cadf29a8243da48ddd0efd
Shamil <shamilyassin799@gmail.com>
2024-03-02 19:40:13 +0700

    Mengubah isi program "ListBarangController" "list_barang" dan web.php

--- app/Http/Controllers/ListBarangController.php
+++ app/Http/Controllers/ListBarangController.php
@@ -6,7 +6,23 @@ use Illuminate\Http\Request;
 
 class ListBarangController extends Controller
 {
-    function tampilkan($id, $nama){
-        return view('list_barang', ['id' => $id, 'nama' => $nama]);
+    public function getData()
+    {
+        // Logika untuk mendapatkan array data
+        $dataBarang = [
+            ['id' => 1, 'nama' => 'KOPI ABC', 'harga' => 20000],
+            ['id' => 2, 'nama' => 'White Coffee', 'harga' => 30000],
+            ['id' => 3, 'nama' => 'ENERGEN', 'harga' => 40000],
+            ['id' => 4, 'nama' => 'MILO', 'harga' => 50000],
+            ['id' => 5, 'nama' => 'POP ICE', 'harga' => 80000],
+        ];
+
+        return $dataBarang;
+    } 
+
+    public function tampilkan(){
+        $data = $this->getData();
+        return view('list_barang', compact('data'));
     }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -4,24 +4,19 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\DashboardController;
-
-// Route::get('/listbarang/{id}/{nama}', function ($id, $nama){
-//     return view('list_barang', compact('id', 'nama'));
-// });
+use App\Http\Controllers\ListBarangController;
 
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login.form');
 Route::post('/login', [LoginController::class, 'processLogin'])->name('login.process');
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/listbarang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
+Route::get('/listbarang', [ListBarangController::class, 'tampilkan']);
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
-Route::get('/listbarang/{id}/{nama}', function ($id, $nama){
-    return view('list_barang', compact('id', 'nama'));
-});
+
 Route::prefix('admin')->group(function () {
     Route::get('/dashboard', function () {
         return 'Admin Dashboard';
