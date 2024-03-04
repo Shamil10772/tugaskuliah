6ec3bafeaf487d01d50d9ff7982bf89da9acf824
Shamil <shamilyassin799@gmail.com>
2024-03-04 21:50:38 +0700

    Berhasil menghubungkan semuanya

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,5 +1,17 @@
-<div>
-    <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
-  Selamat datang di dashboard
-  ini adalah tampilan dashboard
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Go to Next Page</title>
+</head>
+<body>
+  <h1>ini adalah halaman dashboard</h1>
+  <p>Dashboard.</p>
+
+  <!-- Tautan untuk pergi ke halaman selanjutnya -->
+  <a href="http://localhost:8000/listbarang">Go to List barang</a>
+</body>
+</html>
+
+
