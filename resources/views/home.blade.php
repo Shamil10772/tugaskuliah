9372b27df5c30d1eb0868d0a28eb37927a123e87
Shamil <shamilyassin799@gmail.com>
2024-03-08 14:40:03 +0700

    Menambah, Menghapus dan memperbarui

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,9 @@
 <div>
     <!-- Waste no more time arguing what a good man should be, be one. - Marcus Aurelius -->
-  Selamat datang di home
-  ini adalah tampilan home
-  <a href="{{ route('dashboard') }}">Go to Dashboard</a>
+<head>
+  <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+  <h1>Hello, this is laravel CSS home</h1>
+  <img src="{{ asset('gambar/david de gea.JPEG') }}" alt="Laravel" width="300" h>
+  <img src="{{ asset('gambar/david de gea1.JPEG') }}" alt="Laravel" width="300" h>
 </div>
